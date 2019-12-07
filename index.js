b2fa5fb6d26513be186897d6aec5a558a2ed9554
S.Kim <skim1025@gmail.com>
2019-12-07 23:12:35 +0900

    mort from env

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const Datastore = require('nedb');
-const port = 3000;
+const port = process.env.PORT || 3000;
 const fetch = require('node-fetch');
 require('dotenv').config();
 
 //console.log(process.env);
 
 const app = express();
-app.listen(port,()=>console.log("Currently at 3000."));
+app.listen(port, () => {
+    console.log(`starting server at ${port}`)
+});
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
